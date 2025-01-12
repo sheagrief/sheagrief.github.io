3b7a226b33d74bf67f995f76a0571ec99f0e47eb
sheagrief <3a.mad1earth4@gmail.com>
2025-01-13 05:04:10 +0900

    update: post header

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,21 @@
 // Given from https://vitepress.dev/guide/extending-default-theme
 
+import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
+import PostTitle from "./PostTitle.vue";
+import { useData } from "vitepress";
 import "./custom.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => {
+        const { page } = useData();
+        if (page.value.relativePath.match(/^posts\/(?!index.md)/)) {
+          return h(PostTitle);
+        }
+      },
+    });
+  },
+};
